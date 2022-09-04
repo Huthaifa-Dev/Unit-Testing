f486dcd6daa7c23303b1d5b087aca3cb8c8c4307
Huthaifa Salman <H.j.Salman@students.ptuk.edu.ps>
2022-09-04 11:59:58 +0300

    added tests for the functions :fire:

---
+++ data/users.js
@@ -0,0 +1,75 @@
+const data = {
+    "users": [
+        {
+            "name": "John Doe",
+            "age": 30,
+            "occupation": "teacher",
+            "city": "New York",
+            "state": "NY"
+        },
+        {
+            "name": "Jane Smith",
+            "age": 25,
+            "occupation": "nurse",
+            "city": "Los Angeles",
+            "state": "CA"
+        },
+        {
+            "name": "Mike Johnson",
+            "age": 35,
+            "occupation": "construction worker",
+            "city": "Chicago",
+            "state": "IL"
+        },
+        {
+            "name": "Mary Williams",
+            "age": 40,
+            "occupation": "CEO",
+            "city": "Houston",
+            "state": "TX"
+        },
+        {
+            "name": "Robert Brown",
+            "age": 45,
+            "occupation": "lawyer",
+            "city": "Philadelphia",
+            "state": "PA"
+        },
+        {
+            "name": "Laura Jones",
+            "age": 50,
+            "occupation": "engineer",
+            "city": "San Diego",
+            "state": "CA"
+        },
+        {
+            "name": "Kevin Miller",
+            "age": 55,
+            "occupation": "doctor",
+            "city": "Dallas",
+            "state": "TX"
+        },
+        {
+            "name": "Sarah Wilson",
+            "age": 60,
+            "occupation": "writer",
+            "city": "San Francisco",
+            "state": "CA"
+        },
+        {
+            "name": "Adam Moore",
+            "age": 65,
+            "occupation": "artist",
+            "city": "Portland",
+            "state": "OR"
+        },
+        {
+            "name": "Emma Johnson",
+            "age": 70,
+            "occupation": "retired",
+            "city": "Seattle",
+            "state": "WA"
+        }
+    ]
+}
+module.exports = data;
